a2807a173454f7e970d076bb5184e4e4e9178329
corner <corner3499@gmail.com>
2022-05-04 10:38:24 +0900

    main Search Form

--- src/components/home/MainSearchComponent.vue
+++ src/components/home/MainSearchComponent.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="main-search d-block">
-    <h2>오늘은 어디로 가볼까?</h2>
-    <div class="area_select">
-      지역별 &nbsp;&nbsp; <i class="fa fa-angle-down"></i>
+    <h2>{{ searchTitle }}</h2>
+    <div class="area_select" @click="movePage('/')">
+      지역별 &nbsp;&nbsp; <font-awesome-icon icon="fa-solid fa-arrow-down" />
     </div>
     <div class="search_form">
-
+      <el-input type="text" @click.native="changeTitle()"
+                v-model="searchInput" class="search_input"/>
+      <el-button class="search_btn" @click="movePage('/')"><font-awesome-icon icon="fa-solid fa-magnifying-glass" /></el-button>
     </div>
   </div>
 </template>
@@ -21,7 +23,10 @@ export default {
   },
   props: {},
   data() {
-    return {}
+    return {
+      searchTitle: '오늘은 어디로 가볼까?',
+      searchInput: '',
+    }
   },
   beforeRouterEnter() {
   },
@@ -34,7 +39,22 @@ export default {
   destroyed() {
   },
   computed: {},
-  methods: {},
+  methods: {
+    changeTitle() {
+      let titleArr = [
+        '오늘은 어디로 가볼까?', '검색을 해볼까요?', '좋은 곳은 많아요.', '오늘도 좋은 하루 되세요.'
+      ]
+      return this.searchTitle = this.randArr(titleArr);
+    },
+    randArr(array) {
+      let rand = Math.random() * array.length | 0;
+      let rValue = array[rand];
+      return rValue;
+    },
+    movePage(url) {
+      return this.searchTitle = '아직 검색기능은 존재하지 않아요.  '
+    },
+  },
   watch: {},
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -21,10 +21,11 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 
 /* add font awesome icon component */
 library.add(
-  far
+  far, fas
 )
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 window.$ = require('jquery')
