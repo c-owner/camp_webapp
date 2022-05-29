303417164704f6d37bb99c323cdedab9ec094767
Eight-Corner <corner3499@gmail.com>
2022-05-29 13:01:21 +0900

    setup_input_style

--- pages/Auth/Register.vue
+++ pages/Auth/Register.vue
@@ -2,25 +2,25 @@
   <div class="form_main_wrap" id="main_fullbg">
     <div class="mainwrap"></div>
     <div class="form_white">
-      <form @submit.prevent="register">
+      <v-form @submit.prevent="register">
         <div class="form-group">
           <label for="name">사용하실 닉네임</label>
-          <el-input type="text" class="form-control" id="name" required
+          <el-input type="text" class="auth-input" id="name" required
                     v-model="name" placeholder="닉네임을 작성해주세요" />
         </div>
         <div class="form-group">
           <label for="email">사용하실 이메일 (로그인 이메일) </label>
-          <el-input type="email" class="form-control" id="email" required v-model="email" placeholder="email@email.com" />
+          <el-input type="email" class="auth-input" id="email" required v-model="email" placeholder="email@email.com" />
         </div>
         <div class="form-group">
           <label for="password">비밀번호</label>
-          <el-input type="password" class="form-control" id="password" required v-model="password" placeholder="비밀번호 입력" />
+          <el-input type="password" class="auth-input" id="password" loading required v-model="password" placeholder="비밀번호 입력" />
           <label for="password_confirm">비밀번호 확인</label>
-          <el-input type="password" class="form-control" id="password_confirm" required v-model="password_confirm" placeholder="비밀번호 확인" />
+          <el-input type="password" class="auth-input" id="password_confirm" required v-model="password_confirm" placeholder="비밀번호 확인" />
         </div>
         <el-button type="submit" class="btn btn-primary">회원가입</el-button>
         <el-button class="btn btn-primary" @click="$router.replace('/')">취소</el-button>
-      </form>
+      </v-form>
     </div>
   </div>
 </template>
