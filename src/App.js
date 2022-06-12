18059f02bcd33864f533e68c5d45bccee4705a66
Eight-Corner <corner3499@gmail.com>
2022-06-12 13:45:26 +0900

    css fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './assets/css/App.css';
+import './assets/scss/index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./views/Main";
