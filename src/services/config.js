0a278aff0649d05e7daca8a0b0a33262e004808f
Eight-Corner <corner3499@gmail.com>
2022-06-13 21:17:10 +0900

    config api url

--- src/services/config.js
+++ src/services/config.js
@@ -2,11 +2,11 @@ let isDev = true; // true : dev, false; prod
 
 let config = {
     dev: {
-        apiDomain: "http://localhost:8080",
+        apiDomain: "http://ec2-52-79-82-151.ap-northeast-2.compute.amazonaws.com",
         serverDomain: "https://front-campfire-web.vercel.app",
     },
     prod: {
-        apiDomain: "http://localhost:8080",
+        apiDomain: "http://ec2-52-79-82-151.ap-northeast-2.compute.amazonaws.com",
         serverDomain: "https://front-campfire-web.vercel.app/",
     },
 }
