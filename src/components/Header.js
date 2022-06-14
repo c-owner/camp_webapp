033af8e3fc08929e93c4c7598092278e48600943
tjwns3481 <tjwns3481@gmail.com>
2022-06-14 16:33:08 +0900

    220614

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,31 +6,35 @@ class header extends Component {
       <div id='header_wrap'>
         <h1 id='header_main_logo'>
           <a href="#">
-            <h1>mainLogo</h1>
+            <span>mainLogo</span>
           </a>
         </h1>
         
         <ul id='header_menu_wrap'>
           <li id='header_search' className='header_menu'>
             <a href="#">
+              <i class="bi bi-search"></i>
               <span>search</span>
             </a>
           </li>
 
           <li id='header_map' className='header_menu'>
             <a href="#">
+              <i class="bi bi-map"></i>
               <span>map</span>
             </a>
           </li>
 
           <li id='header_menubar' className='header_menu'>
             <a href="#">
+              <i class="bi bi-list"></i>
               <span>menubar</span>
             </a>
           </li>
 
           <li id='header_signIn' className='header_menu'>
             <a href="#">
+              <i class="bi bi-person"></i>
               <span>signIn/signUp</span>
             </a>
           </li>
---
+++ src/views/Main.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+
+export default class Main extends Component {
+  render() {
+    return (
+      <>
+      <div id='main_wrap'>
+        <section id='banner_section'>
+          배너 색션
+        </section>
+        <section id='region_section'>
+          지역 색션
+        </section>
+        <section id='map_section'>
+          지도 색션
+        </section>
+      </div>
+      </>
+    )
+  }
+}
