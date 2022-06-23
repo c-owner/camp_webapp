42766adf0d7ed7cd67268d4eb88a285a98a509ab
tjwns3481 <tjwns3481@gmail.com>
2022-06-23 16:20:05 +0900

    220623 메인 퍼블리싱

--- src/components/TagSectionComponent.jsx
+++ src/components/TagSectionComponent.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TagSectionComponent = () => {
   let regionId = 0
   const regions = ['전국', '서울', '인천', '경기', '충북', '충남', '세종', '강원', '대전', '경북', '전북', '대구', '경남', '광주', '전남', '울산', '부산', '제주']
   const regionList = regions.map((region) => (
-    <li className="tag-item-box" key={regionId++}>
+    <Link to="/search"><li className="tag-item-box" key={regionId++}>
       <span className="tag-item">{region}</span>
-    </li>
+    </li></Link>
   ))
   return (
     <div>
