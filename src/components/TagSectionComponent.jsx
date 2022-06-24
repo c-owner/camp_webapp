f51a0dc70ea6af37107be980b79b1fa417357fb5
tjwns3481 <tjwns3481@gmail.com>
2022-06-24 15:29:35 +0900

    220624

--- src/components/TagSectionComponent.jsx
+++ src/components/TagSectionComponent.jsx
@@ -3,6 +3,26 @@ import { Link } from 'react-router-dom';
 
 const TagSectionComponent = () => {
   let regionId = 0
+  // {
+  //   korea : '전국',
+  //   seoul : '서울',
+  //   incheon : '인천',
+  //   kyungki: '경기',
+  //   chungbuk: '충북',
+  //   chungnam: '충남',
+  //   sejong: '세종',
+  //   kangwon: '강원',
+  //   daejeon: '대전',
+  //   kyungbuk : '경북',
+  //   jeonbuk: '전북',
+  //   daegu: '대구',
+  //   kyungnam: '경남',
+  //   gwangju: '광주',
+  //   jeonnam: '전남',
+  //   ulsan: '울산',
+  //   busan: '부산',
+  //   jeju : '제주'
+  // }
   const regions = ['전국', '서울', '인천', '경기', '충북', '충남', '세종', '강원', '대전', '경북', '전북', '대구', '경남', '광주', '전남', '울산', '부산', '제주']
   const regionList = regions.map((region) => (
     <Link to="/search"><li className="tag-item-box" key={regionId++}>
--- src/layouts/SectionComponent.jsx
+++ src/layouts/SectionComponent.jsx
@@ -16,7 +16,7 @@ const SectionComponent = (props) => {
     }
   }
   return (
-    <div className="section-box pa15 mb20">
+    <div className="section-box pa15 mb15">
       <div className='section-box-top mb15'>
         <h3 className="section-title in_block">{props.title}</h3>
         <Link to="/search">
